84838712c97f8f49a8c4b8e25530f0208782843a
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-01-15 20:37:15 -0600

    2. Changed text and color

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'How many times can you stand to push the button?:',
             ),
             Text(
               '$_counter',
